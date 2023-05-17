9a6b9d9729b0a6444aa462033d41fd626d8eddc8
SamanthaPersad <39538222+SamanthaPersad@users.noreply.github.com>
2023-05-18 00:49:32 +0500

    Add files via upload

---
+++ NTierApp.Business/OrdersBusiness.cs
@@ -0,0 +1,101 @@
+﻿using NTier.Domain;
+using NtierApp.Repository.Infrastucture.Contract;
+using NtierApp.Repository.Repositories;
+using NTierApp.Business.Interface;
+using System;
+using System.Collections.Generic;
+
+namespace NTierApp.Business
+{
+    public class OrdersBusiness : IOrdersBusiness
+    {
+
+        private readonly IUnitOfWork unitOfWork;
+        private readonly OrderRepository _orderRepository;
+        private readonly IProductBusiness _productBusiness;
+
+        public OrdersBusiness(IUnitOfWork _unitOfWork, IProductBusiness productBusiness)
+        {
+            this.unitOfWork = _unitOfWork;
+            this._productBusiness = productBusiness;
+            this._orderRepository = new OrderRepository(unitOfWork);
+        }
+        public IEnumerable<Orders> GetAll()
+        {
+            var result = _orderRepository.GetAll();
+            return result;
+        }
+        public IEnumerable<Orders> GetAllConfirmedOrders()
+        {
+            var result = _orderRepository.GetAll(x => x.ConfirmStatus == true);
+            return result;
+        }
+
+        public IEnumerable<Orders> GetMyOrders(string userId)
+        {
+            var result = _orderRepository.GetAll(x => x.UserId == userId);
+            return result;
+        }
+
+        public IEnumerable<Orders> GetAllUnConfirmedOrders()
+        {
+            var result = _orderRepository.GetAll(x => x.ConfirmStatus == false);
+            return result;
+        }
+
+        public Orders GetById(int orderId)
+        {
+            Orders result = null;
+            if (orderId != null)
+                result = _orderRepository.GetById(x => x.Id == orderId);
+
+            return result;
+        }
+
+        public void Insert(Orders order)
+        {
+
+            if (order != null)
+            {
+
+                order.OrderDate = DateTime.Now;
+                order.ConfirmStatus = false;
+                order.ConfirmDate = DateTime.Now;
+                var product = _productBusiness.GetById(order.ProductId);
+                product.Quantity--;
+                _orderRepository.Insert(order);
+                _productBusiness.Update(product);
+            }
+        }
+
+        public void Update(Orders order)
+        {
+            if (order != null)
+            {
+                order.OrderDate = DateTime.Now;
+                order.ConfirmDate = DateTime.Now;
+                order.ConfirmStatus = false;
+                _orderRepository.Update(order);
+            }
+        }
+
+        public void Delete(int orderId)
+        {
+            if (orderId != null)
+                _orderRepository.Delete(x => x.Id == orderId);
+        }
+
+        public void ConfirmOrder(int orderId)
+        {
+            if (orderId != null)
+            {
+                var result = _orderRepository.GetById(x => x.Id == orderId);
+                result.ConfirmStatus = true;
+                result.ConfirmDate = DateTime.Now;
+                _orderRepository.Update(result);
+            }
+        }
+
+       
+    }
+}
---
+++ NTierApp.Business/PurchaseBusiness.cs
@@ -0,0 +1,100 @@
+﻿using NTier.Domain;
+using NtierApp.Repository;
+using NtierApp.Repository.Infrastucture.Contract;
+using NtierApp.Repository.Repositories;
+using NTierApp.Business.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierApp.Business
+{
+    public class PurchaseBusiness : IPurchaseBusiness
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly PurchaseRepository _purchaseRepository;
+        private readonly ProductRepository _productRepository;
+
+        public PurchaseBusiness(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+            this._purchaseRepository = new PurchaseRepository(_unitOfWork);
+            this._productRepository = new ProductRepository(_unitOfWork);
+        }
+
+        public IEnumerable<Purchase> GetAll()
+        {
+            var result = _purchaseRepository.GetAll();
+            return result;
+        }
+
+        public Purchase GetById(int purchaseId)
+        {
+            Purchase result = null;
+            if (purchaseId != null)
+                result = _purchaseRepository.GetById(x => x.Id == purchaseId);
+            return result;
+        }
+
+        public void Insert(Purchase purchase)
+        {
+            if (purchase != null)
+            {
+                purchase.CreatedTime = DateTime.Now;
+                _purchaseRepository.Insert(purchase);
+            }
+        }
+
+        public void Update(Purchase purchase)
+        {
+            if (purchase != null)
+            {
+                _purchaseRepository.Update(purchase);
+            }
+        }
+
+        public void Delete(int purchaseId)
+        {
+            if (purchaseId != null)
+            {
+                var purchase = _purchaseRepository.GetById(x => x.Id == purchaseId);
+                _purchaseRepository.Delete(x => x.Id == purchaseId);
+
+                var product = _productRepository.GetById(p => p.Id == purchase.ProductId);
+                product.Quantity -= purchase.Quantity;
+                _productRepository.Update(product);
+            }
+        }
+
+        public void ConfirmPurchase(int purchaseId)
+        {
+            if(purchaseId != null)
+            {
+                var purchase = _purchaseRepository.GetById(p => p.Id == purchaseId);
+                purchase.Confirmation = true;
+                purchase.ConfirmationTime = DateTime.Now;
+                _purchaseRepository.Update(purchase);
+
+                var product = _productRepository.GetById(p=> p.Id == purchase.ProductId);
+                product.Quantity += purchase.Quantity;
+                _productRepository.Update(product); 
+            }
+        }
+
+        public void UnconfirmPurchase(int purchaseId)
+        {
+            if (purchaseId != null)
+            {
+                var purchase = _purchaseRepository.GetById(p => p.Id == purchaseId);
+                purchase.Confirmation = false;
+                _purchaseRepository.Update(purchase);
+
+                var product = _productRepository.GetById(p => p.Id == purchase.ProductId);
+                product.Quantity -= purchase.Quantity;
+                _productRepository.Update(product);
+            }
+        }
+    }
+}
